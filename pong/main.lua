e528bb2bad46977849b02aaf9f96d4c7acc74f93
Simon Anderson <simon.anderson111@gmail.com>
2023-04-01 23:33:08 +1100

    Moved all tracking to classes

--- pong/main.lua
+++ pong/main.lua
@@ -54,21 +54,27 @@ end
 function love.update(dt)
 
     if love.keyboard.isDown('w') then
-        player1Y = math.max(0, player1Y + -PADDLE_SPEED * dt)
+        player1.dy = -PADDLE_SPEED
     elseif love.keyboard.isDown('s') then
-        player1Y = math.min(VIRTUAL_HEIGHT - 20, player1Y + PADDLE_SPEED * dt)
+        player1.dy = PADDLE_SPEED
+    else
+        player1.dy = 0
     end
 
     if love.keyboard.isDown('up') then
-        player2Y = math.max(0, player2Y + -PADDLE_SPEED * dt)
+        player2.dy = -PADDLE_SPEED
     elseif love.keyboard.isDown('down') then
-        player2Y = math.min(VIRTUAL_HEIGHT - 20, player2Y + PADDLE_SPEED * dt)
+        player2.dy = PADDLE_SPEED
+    else
+        player2.dy = 0
     end
 
     if gameState == 'play' then
-        ballX = ballX + ballDX * dt
-        ballY = ballY + ballDY * dt
+        ball:update(dt)        
     end        
+
+    player1:update(dt)
+    player2:update(dt)
 
 end
 
@@ -82,13 +88,8 @@ function love.keypressed(key)
         else
             gameState = 'start'
 
-            -- Center the ball
-            ballX = VIRTUAL_WIDTH / 2 - 2
-            ballY = VIRTUAL_HEIGHT / 2 - 2
+            ball:reset()
 
-            -- Ball velocity calibration
-            ballDX = math.random(2) == 1 and 100 or -100
-            ballDY = math.random(-50, 50) * 1.5
         end
     end
 end
@@ -108,19 +109,12 @@ function love.draw()
         love.graphics.printf('Hello Play State!', 0, 20, VIRTUAL_WIDTH, 'center')
     end
 
-    --Score
-    love.graphics.setFont(scoreFont)
-    love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 3)
-    love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 3)
+    -- Paddle state
+    player1:render()
+    player2:render()
 
-    -- Paddle 1 render
-    love.graphics.rectangle('fill', 10, player1Y, 5, 20)
-
-    -- Paddle 2 render
-    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, player2Y, 5, 20)
-
-    -- Ball render
-    love.graphics.rectangle('fill', ballX, ballY, 4, 4)
+    -- Ball state
+    ball:render()
 
     push:apply('end')
 end
